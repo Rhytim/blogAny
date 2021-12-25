8f3ddacf39798caf0594c643e3de0ed10006cac5
Marc Lopez <marclopezmoll@mbp2022.home>
2021-12-25 23:43:39 +0100

    styled logo+nav and content in progress

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <title>Blog de Videojuegos</title>
 </head>
 
@@ -43,6 +44,7 @@
                 </li>
             </ul>
         </nav>
+        <div class="clearfix"></div>
     </header>
 
     <!-- BARRA LATERAL -->
